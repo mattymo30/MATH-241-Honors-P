47fc1008aaa1edfdbebb348d907947eb5ec0bf76
Matthew Morrison <msm8275@rit.edu>
2024-04-18 00:58:17 -0400

    Started new implementation using armadillo instead of Matlab, added implementation to add fluff to messgae if not divisible by 3

--- HillCipher/HillCipher/HillCipher.cpp
+++ HillCipher/HillCipher/HillCipher.cpp
@@ -1,11 +1,124 @@
-// HillCipher.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <armadillo>
+#include <cstring>
+#include "hill_c.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+using namespace arma;
+
+/**
+ * Convert the key matrix into a vector for future use
+ *
+ * @param key the key matrix to convert
+ * @return a vector version of the key matrix
+*/
+vector<float> key_to_vector(float key[][3]) {
+    vector<float> keyVector;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            keyVector.push_back(key[i][j]);
+        }
+    }
+
+    return keyVector;
+}
+
+/**
+ * Using matlab, check if the key has an inverse
+ *
+ * @param key the vector version of the key matrix
+ * @return True if the key has a matrix or False otherwise
+*/
+extern bool has_inverse(std::vector<int> key) {
+
+    return true;
+}
+
+
+/**
+ * Encrypt the message using the Hill Cipher
+ *
+ * @param message a vector of vectors representation of the message grouped
+ * by three characters together
+ * @param key the vector version of the key matrix
+ *
+ * @return the encrypted message
+*/
+void encrypt_hill_cipher(vector<vector<int>> message, vector<int> key) {
+}
+
+/**
+ * Using the inverse key matrix decrypt the encrypted messgae back into its
+ * original state
+ *
+ * @param encrypted a vector of vectors representation of the
+ * encrypted message grouped by three characters together
+ * @param inverse_key the inverse representation of the key matrix
+ *
+ * @return the decrypted messgae in vector form
+*/
+void decrypt_hill_cipher(vector<vector<int>> encrpted, vector<int> inverse_key) {
+}
+
+
+
+
+
+
+
+
+int main() {
+    cout << "Enter your message: ";
+    string message;
+    getline(cin, message);
+    // transform message into uppercase for simplicity
+    transform(message.begin(), message.end(), message.begin(), toupper);
+
+    int message_len = message.length();
+    int rem = message_len % 3;
+    // pad message with As if message not divisible by 3
+    if (rem != 0) {
+        int to_add = 3 - rem;
+        for (int i = 0; i < to_add; i++) {
+            message.push_back('A');
+        }
+    }
+
+   
+    mat key(3, 3);
+    key.zeros();
+
+    /**
+    float key[3][3];
+    vector<vector<float>> key_vector;
+    char* key_arr = new char[9 + 1];
+    bool inverse = false;
+    do {
+        string init_key = "";
+        cout << "Enter 9 Letters to be the Key (no spaces): ";
+        cin >> init_key;
+
+        int key_len = init_key.length();
+        if (key_len != 9) {
+            cout << "Key does not have a length of 9!\n";
+            continue;
+        }
+        // transform to uppercase for simplicity
+        transform(init_key.begin(), init_key.end(), init_key.begin(), toupper);
+
+        strcpy(key_arr, init_key.c_str());
+
+
+
+    } while (!inverse);
+      
+    delete[] key_arr;
+    */
+
+
+    return 0;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ HillCipher/HillCipher/hill_c.h
@@ -0,0 +1,5 @@
+#ifndef HILL_CIPHER
+#define HILL_CIPHER
+#include <vector>
+extern bool has_inverse(std::vector<int> key);
+#endif
